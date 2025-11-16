16041c5fd82cfb52162664a12e6b4fd5357e7b42
Inaya Merchant <gmerchant777@gmail.com>
2025-11-15 19:32:28 -0800

    does this work?

---
+++ button_test/button_test.ino
@@ -0,0 +1,30 @@
+/*
+ Reads the push-button state
+*/
+
+const int buttonPin = 12;
+int buttonState = 0; // stores the state of the button
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  buttonState = digitalRead(buttonPin); //checks if button is pressed
+
+  if ( buttonState ==  HIGH )
+  {
+    Serial.println("PRESSED");
+  }
+  else
+  {
+    Serial.println("not pressed");
+  }
+
+  delay(500);
+}
